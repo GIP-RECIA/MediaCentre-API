3bd4fa41b3b7a341b0ba6d7e775d00011cb9f9b7
Manon Cornu <manon.cornu@recia.fr>
2024-04-19 16:13:19 +0200

    ci: add cors configuration and mediacentre configuration

---
+++ src/main/java/fr/recia/mediacentre/mediacentre/configuration/CorsConfiguration.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright © ${project.inceptionYear} GIP-RECIA (https://www.recia.fr/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.recia.mediacentre.mediacentre.configuration;
+
+import fr.recia.mediacentre.mediacentre.configuration.bean.CorsProperties;
+import fr.recia.mediacentre.mediacentre.configuration.bean.MediaCentreProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Slf4j
+@Configuration
+public class CorsConfiguration {
+
+
+    private final CorsProperties corsProperties;
+
+  public CorsConfiguration(MediaCentreProperties mediaCentreProperties) {
+    this.corsProperties = mediaCentreProperties.getCors();
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+    if (log.isWarnEnabled()) log.warn("CORS: {}", corsProperties.isEnable());
+    if (corsProperties.isEnable()) {
+      org.springframework.web.cors.CorsConfiguration configuration = new org.springframework.web.cors.CorsConfiguration();
+
+      configuration.setAllowCredentials(corsProperties.isAllowCredentials());
+      configuration.setAllowedOrigins(corsProperties.getAllowedOrigins());
+      configuration.setExposedHeaders(corsProperties.getExposedHeaders());
+      configuration.setAllowedHeaders(corsProperties.getAllowedHeaders());
+      configuration.setAllowedMethods(corsProperties.getAllowedMethods());
+
+      source.registerCorsConfiguration("/**", configuration);
+    }
+
+    return source;
+  }
+}
+
---
+++ src/main/java/fr/recia/mediacentre/mediacentre/configuration/bean/MediaCentreProperties.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright © ${project.inceptionYear} GIP-RECIA (https://www.recia.fr/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.recia.mediacentre.mediacentre.configuration.bean;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+@ConfigurationProperties(
+  prefix = "app",
+  ignoreUnknownFields = false
+)
+@Data
+//@Validated
+@Slf4j
+public class MediaCentreProperties {
+
+  private CorsProperties cors = new CorsProperties();
+
+  @Override
+  public String toString() {
+    return "{\n"
+      + cors + ",\n"
+      + "\n}";
+  }
+}
+
