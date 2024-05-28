9a2d5f1e17a2e70fdebe38f9b20b6428e48de58b
Manon Cornu <manoncornu45@icloud.com>
2024-05-28 16:16:39 +0200

    feat: add FilterEnum & CategoriesByProfilesProperties

---
+++ src/main/java/fr/recia/mediacentre/mediacentre/model/filter/FilterEnum.java
@@ -0,0 +1,8 @@
+package fr.recia.mediacentre.mediacentre.model.filter;
+
+public enum FilterEnum {
+    UAI_FILTER,
+    NIVEAU_EDUCATIF_FILTER,
+    TYPE_PRESENTATION_FILTER,
+    DOMAINE_ENSEIGNEMENT_FILTER
+}
