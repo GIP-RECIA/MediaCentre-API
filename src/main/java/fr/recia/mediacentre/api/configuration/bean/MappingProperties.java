52cc12a53f810f291b1f1465fda9c107623634bd
Ewann ROUX <ewann.roux@recia.fr>
2025-02-21 12:02:38 +0100

    feat: add checks in PostConstruct to prevent startup if properties are invalids

--- src/main/java/fr/recia/mediacentre/api/configuration/bean/MappingProperties.java
+++ src/main/java/fr/recia/mediacentre/api/configuration/bean/MappingProperties.java
@@ -20,15 +20,21 @@ import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
 import javax.annotation.PostConstruct;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
 @Data
 @Slf4j
 @Setter(AccessLevel.PUBLIC)
@@ -37,22 +43,75 @@ import java.util.List;
 @ConfigurationProperties(prefix = "mapping")
 public class MappingProperties {
 
-  //  @Value("${mapping.soffit.userinfoAttributes}")
-//  @NotNull @NotEmpty
+
+  @Autowired
+  private ApplicationContext ctx;
+
+
+  @NotNull @NotEmpty
   private List<UserInfoAttribute> userinfoAttributes;
 
-  //  @Value("${mapping.soffit.profileKey}")
-//  @NotNull
-  private String profileKey;
 
-  //  @Value("${mapping.payload.groups.out}")
-//  @NotNull
+  private String profileKey;
+  private String currentEtabUaiKey;
+
+  @NotNull
   private UserInfoAttribute groups;
 
 
   @PostConstruct
   private void init() {
+
+    if(this.groups.getOut().trim().isEmpty()){
+      cancelServerStart("Parameter {out} of {groups} in MappingProperties is empty, startup canceled.");
+    }
+
+    if(this.profileKey.trim().isEmpty()){
+      cancelServerStart("Parameter {profileKey} in MappingProperties is empty, startup canceled.");
+    }
+    if(this.currentEtabUaiKey.trim().isEmpty()){
+      cancelServerStart("Parameter {currentEtabUaiKey} in MappingProperties is empty, startup canceled.");
+    }
+
+    Set<String> inKeys = new HashSet<>();
+    Set<String> outKeys = new HashSet<>();
+
+    for(UserInfoAttribute userInfoAttribute: this.userinfoAttributes){
+      if(Objects.isNull(userInfoAttribute.getIn()) || userInfoAttribute.getIn().trim().isEmpty()){
+        cancelServerStart("Parameter {in} of a member of {userinfoAttributes} in MappingProperties is empty, startup canceled.");
+      }
+      if(Objects.isNull(userInfoAttribute.getOut()) || userInfoAttribute.getIn().trim().isEmpty()){
+        cancelServerStart("Parameter {out} of a member of {userinfoAttributes} in MappingProperties is empty, startup canceled.");
+      }
+
+      boolean addedIn = inKeys.add(userInfoAttribute.getIn());
+      boolean addedOut = outKeys.add(userInfoAttribute.getOut());
+
+      if(!addedIn){
+        cancelServerStart(String.format("the {in} key {%s} of a member of {userinfoAttribues} in MappingProperties is present multiple time, startup canceled", userInfoAttribute.getIn()));
+      }
+      if(!addedOut){
+        cancelServerStart(String.format("the {out} key {%s} of a member of {userinfoAttribues} in MappingProperties is present multiple time, startup canceled", userInfoAttribute.getOut()));
+      }
+    }
+
+    if(!outKeys.contains(this.profileKey)){
+      cancelServerStart(String.format("Parameter {profileKey} : {%s} in MappingProperties does not match any out key of {groups}, startup canceled.", this.profileKey));
+    }
+    if(!outKeys.contains(this.currentEtabUaiKey)){
+      cancelServerStart(String.format("Parameter {currentEtabUaiKey} : {%s} in MappingProperties does not match any out key of {groups}, startup canceled.", this.currentEtabUaiKey));
+    }
+    if(this.profileKey.equals(this.currentEtabUaiKey)){
+      cancelServerStart(String.format("Parameter {profileKey} : {%s} and {currentEtabUaiKey} : {%s} in MappingProperties are the same, startup canceled.", this.profileKey, this.currentEtabUaiKey));
+    }
+
     log.info("Loaded properties: {}", this);
+  }
+
+  private void cancelServerStart(String exMessage){
+    log.error(exMessage);
+    ((ConfigurableApplicationContext) ctx).close();
+    System.exit(1);
   }
 
   @Override
