c8e52594c8284b8bf44502a1db7ba460ebe70eed
Ewann ROUX <ewann.roux@recia.fr>
2025-04-16 10:33:08 +0200

    chore: add test profile annotation

--- src/test/java/fr/recia/mediacentre/api/MediacentreApplicationTest.java
+++ src/test/java/fr/recia/mediacentre/api/MediacentreApplicationTest.java
@@ -20,11 +20,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 class MediacentreApplicationTest {
 
 	@MockBean
