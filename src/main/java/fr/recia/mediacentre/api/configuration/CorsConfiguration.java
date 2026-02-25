39ceef1574f247e5af95f0288a003817779e5b0a
Ewann ROUX <ewann.roux@recia.fr>
2026-02-25 10:56:15 +0100

    fix: fix CORS configuration

--- src/main/java/fr/recia/mediacentre/api/configuration/CorsConfiguration.java
+++ src/main/java/fr/recia/mediacentre/api/configuration/CorsConfiguration.java
@@ -18,14 +18,13 @@ package fr.recia.mediacentre.api.configuration;
 import fr.recia.mediacentre.api.configuration.bean.CorsProperties;
 import fr.recia.mediacentre.api.configuration.bean.MediaCentreProperties;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Slf4j
 @Configuration
-public class CorsConfiguration {
+public class CorsConfiguration implements WebMvcConfigurer {
 
 
     private final CorsProperties corsProperties;
@@ -34,24 +33,13 @@ public class CorsConfiguration {
     this.corsProperties = mediaCentreProperties.getCors();
   }
 
-  @Bean
-  CorsConfigurationSource corsConfigurationSource() {
-    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-
-    if (log.isWarnEnabled()) log.warn("CORS: {}", corsProperties.isEnable());
-    if (corsProperties.isEnable()) {
-      org.springframework.web.cors.CorsConfiguration configuration = new org.springframework.web.cors.CorsConfiguration();
-
-      configuration.setAllowCredentials(corsProperties.isAllowCredentials());
-      configuration.setAllowedOrigins(corsProperties.getAllowedOrigins());
-      configuration.setExposedHeaders(corsProperties.getExposedHeaders());
-      configuration.setAllowedHeaders(corsProperties.getAllowedHeaders());
-      configuration.setAllowedMethods(corsProperties.getAllowedMethods());
-
-      source.registerCorsConfiguration("/**", configuration);
-    }
-
-    return source;
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+      .allowedOrigins(corsProperties.getAllowedOrigins().toArray(new String[0]))
+      .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+      .allowedHeaders("*")
+      .allowCredentials(true);
   }
 }
 
